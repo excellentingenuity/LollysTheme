d453b5fcf8b9f6fb6ba91f91198c42bb8bd92671
James Johnson <james.johnson@excellentingenuity.com>
2014-08-06 16:11:25 -0400

    begining page structure for home page

--- includes/tabs/home-page.php
+++ includes/tabs/home-page.php
@@ -1,6 +1,22 @@
 <?php
 
 	// ***** Home Page tab *****
-	
+	$tab_1 = $panel->createTab(array('name' => 'Home Page Options') );
+
+	// Menu
+	$tab_1->createOption(array(
+		'name' => 'Top Menu',
+		'id' => 'top-menu',
+		'type' => 'text',
+		'desc' => 'Enter the name of the menu you wish to be used for the Top Menu',
+		'default' => 'Primary'
+	));
+
+	//Header Slideshow
+	$tab_1->createOption( array('name' => 'Header Slideshow Image 1', 'id' => 'header_slideer_image_1', 'type' => 'upload', 'desc' => 'Upload your image') );
+	$tab_1->createOption( array('name' => 'Header Slideshow Image 2', 'id' => 'header_slideer_image_2', 'type' => 'upload', 'desc' => 'Upload your image') );
+	$tab_1->createOption( array('name' => 'Header Slideshow Image 3', 'id' => 'header_slideer_image_3', 'type' => 'upload', 'desc' => 'Upload your image') );
+	$tab_1->createOption( array('name' => 'Header Slideshow Image 4', 'id' => 'header_slideer_image_4', 'type' => 'upload', 'desc' => 'Upload your image') );
+	$tab_1->createOption( array('name' => 'Header Slideshow Image 5', 'id' => 'header_slideer_image_5', 'type' => 'upload', 'desc' => 'Upload your image') );
 
 ?>
