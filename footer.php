608de278578eb156d319eb4ea60c5de285609090
James Johnson <james.johnson@excellentingenuity.com>
2014-08-07 18:05:31 -0400

    working auto copyright notice

--- footer.php
+++ footer.php
@@ -5,9 +5,33 @@
 		<div class="row">
 			<footer>
 				<div class="footer">
-					
+					<div class="col-sm-3">
+						<div class="footer-socialmedia">
+							
+						</div>
+					</div>
+					<div class="col-sm-3">
+						<div class="footer-contactlolly">
+							
+						</div>
+					</div>
+					<div class="col-sm-3">
+						<div class="footer-worldmap">
+							
+						</div>
+					</div>
 				</div>
 			</footer>
+		</div>
+		<div class="row">
+			<div class="copyright-notices-bar">
+				<div class="col-sm-12 copyright">
+					&copy; <?php echo date("Y") ?>
+				</div>
+				<div class="col-sm-12 notices">
+					
+				</div>
+			</div>
 		</div>
 	</div>
 
